9705776131e68c19f142be852c17ef7b888e15b2
Lucas0M <contatolucasmacarini@gmail.com>
2026-03-21 11:22:30 -0300

    aula 09 getelements

---
+++ Javascript/aula009/script.js
@@ -0,0 +1,10 @@
+let body = document.body
+let p1 = window.document.getElementsByTagName('p')[0]
+let d = document.querySelectorAll('p')[1]
+
+
+d.style.background = "green";
+
+console.log(`segundo paragrafo: ${d.innerHTML}`)
+
+// window.alert(p1.innerHTML)
