1d3b22ade444ce5bb9709157c64085f88f85e302
Lucas0M <contatolucasmacarini@gmail.com>
2026-03-23 09:22:40 -0300

    Create ex008.js

---
+++ Javascript/aula011/ex008.js
@@ -0,0 +1,6 @@
+let vel = 12.5
+console.log(`A vel do seu carro e de ${vel}`)
+if (vel > 60){ // condicao simples
+  console.log('Voce ultrapassou a velocidade limite')
+}
+console.log('Dirija com cuidado')
