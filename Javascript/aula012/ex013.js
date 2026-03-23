11a237fbb354a3e09d9277b51204e58d26ecf7c5
Lucas0M <contatolucasmacarini@gmail.com>
2026-03-23 10:48:19 -0300

    switch case Aula 013

---
+++ Javascript/aula012/ex013.js
@@ -0,0 +1,29 @@
+let agora = new Date()
+let diaSem = agora.getDay()
+
+switch(diaSem){
+  case 0:
+    console.log("Domingo!")
+    break
+  case 1:
+    console.log("Segunda-Feira!")
+    break
+  case 2:
+    console.log("Terca-Feira!")
+    break
+  case 3:
+    console.log("Quarta-Feira!")
+    break
+  case 4:
+    console.log("Quinta-Feira!")
+    break
+  case 5:
+    console.log("Sexta-Feira!")
+    break
+  case 6:
+    console.log("Sabadao!")
+    break
+  default:
+    console.log("Nao e um dia da semana (informe um num 1-7)")
+    break
+}
