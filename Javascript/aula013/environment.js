a5cf86308e7b58109c9fc7a42850ccaf377c66de
Lucas0M <contatolucasmacarini@gmail.com>
2026-03-26 12:23:10 -0300

    Create environment.js

---
+++ Javascript/aula013/environment.js
@@ -0,0 +1,19 @@
+// console.log('oi')
+// console.log('oi')
+// console.log('oi')
+// console.log('oi')
+// console.log('oi')
+// console.log('oi')
+// console.log('oi')
+
+let c = 1
+
+// while (c <= 100){
+//   console.log(`passo ${c}`)
+//   c++;
+// }
+
+do {
+  console.log(`passo ${c}`)
+  c++;
+} while (c <= 10)
