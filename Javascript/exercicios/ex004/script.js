eee4ecca8ebabc106f8d380846c1ef8ae8064bba
Lucas0M <contatolucasmacarini@gmail.com>
2026-03-31 10:54:01 -0300

    tabuadas em JS ex004

--- Javascript/exercicios/ex004/script.js
+++ Javascript/exercicios/ex004/script.js
@@ -1,46 +1,20 @@
-let date = new Date()
-let atualAno = date.getFullYear()
+let num = document.getElementById('txtn')
+let tab = document.getElementById('seltab')
 
-let img = document.getElementById('foto')
 
-let ano = document.getElementById('txtano')
-let radsex = document.getElementsByName('radsex') 
-
-let res = document.querySelector('#res p')
-console.log(res)
-
-let genero = ''
-
-function sexo(idade){
-  if(radsex[0].checked){
-    genero = 'man'
-    if (idade >= 0 && idade < 10){
-      img.src = "./imgs/foto-homem-bebe.webp"
-    }else if (idade < 40){
-      img.src = './imgs/foto-homem-adulto.webp'
-    } else {
-      img.src = "./imgs/foto-homem-velho.webp"
-    }
-  } else {
-    genero = 'women'
-    if (idade >= 0 && idade < 10){
-      img.src = "./imgs/foto-mulher-bebe.webp"
-    }else if (idade < 40){
-      img.src = './imgs/foto-mulher-adulta.webp'
-    } else {
-      img.src = "./imgs/foto-mulher-velha.webp"
+function Tabuada() { 
+  if (num.value.length == 0){
+    alert('Por favor insira um valor valido!')
+  }else {
+    let n = Number(num.value)
+    let c = 1
+    tab.innerHTML = ``
+    while(c <= 10){
+      let item = document.createElement('option')
+      item.text = `${n} X ${c} = ${n*c}`
+      item.value = `tab${c}`
+      tab.appendChild(item)
+      c++;
     }
   }
-}
-
-function verificar(){
-  if (ano.value > atualAno){
-    res.innerHTML = "ERROR! You dont born yet."
-  } else if(ano.value < 1900 || ano.value === null){
-    res.innerHTML = "ERROR! We cant calculated this year."
-  } else {
-    let idade = atualAno - ano.value
-    sexo(idade)
-    res.innerHTML = `You are a ${genero} and have ${idade} years old`
-  }
-}
+ }
